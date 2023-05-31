da47a5719649d747a75d590ab0884090e65b7cff
Henry Edmeades <henry@capitalroad.com.au>
2023-05-31 23:00:01 +1000

    router change

--- src/main.jsx
+++ src/main.jsx
@@ -1,35 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import {BrowserRouter, Route, Switch,} from "react-router-dom";
 import './index.css'
 import Home from "./main/Home.jsx";
 import Calculator from "./main/Calculator.jsx";
 import Sport from "./main/Sport.jsx";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/arb-betting",
-    element: <Home />,
-  },
-  {
-    path: "/calculator",
-    element: <Calculator />,
-  },
-  {
-    path: "/sport/:sportId",
-    element: <Sport />,
-  },
-]);
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <BrowserRouter >
+      <Switch>
+        <Route component={Home} exact path="/" />
+
+        <Route component={Home} exact path="/arb-betting" />
+
+        <Route component={Calculator} exact path="/calculator" />
+
+        <Route component={Sport} exact path="/sport/:sportId" />
+      </Switch>
+    </BrowserRouter>
   </React.StrictMode>,
 )
